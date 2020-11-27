7d947937e78f2f71d47eb63b54674f6614fd55f9
Nishit Modha <nishitmodha30@gmail.com>
2020-11-27 15:49:28 +0530

    Added some validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
     validates :user_id, presence: true
+    validates :image, presence: true 
+    validates :caption, presence: true, length: {minimum:3 ,maximum: 255}
     has_one_attached :image 
     belongs_to :user
     has_many :comments, dependent: :destroy
