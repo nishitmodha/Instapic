c52f21b32695c4d362b5a9f8cdd0b0b683756cb8
Nishit Modha <nishitmodha30@gmail.com>
2021-08-27 11:06:01 +0530

    Added design for default profile image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,9 @@
 module ApplicationHelper
-    def profile_avatar_select(user) 
-        if user.avatar.attached?
-        image_tag user.avatar, id: 'image-preview',
-                                        class: 'img-responsive img-circle profile-image'
-                                        else
-                                            image_tag 'default-avatar.jpg'
-                                        end
-                                        
-      end
+  def profile_avatar_select(user) 
+    if user.avatar.attached?
+      image_tag user.avatar, id: 'image-preview', class: 'img-responsive img-circle profile-image'
+    else
+      image_tag 'default-avatar.jpg', class: 'img-responsive img-circle profile-image'
+    end
+  end
 end
-
